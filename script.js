7480e40171f094f2db5666afa9d5886b419ca1d7
FujisanRealEstate <154996184+Imath20@users.noreply.github.com>
2025-05-15 01:59:54 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,180 @@
+const contents = {
+  definitie: {
+    title: "Definiție Simbolism",
+    text: `<p>Simbolismul este  un curent literar apărut în Franța în a doua jumătate a secolului al XIX-lea. Este, mai profund decât orice etichetă academică, un mod de a privi lumea cu ochii închiși, dar cu sufletul larg deschis.</p> <p>Este încercarea poetului de a pătrunde dincolo de suprafață, de a atinge esențele nevăzute ale lucrurilor, acolo unde tăcerea capătă glas, iar umbrele devin cuvinte.
+Simbolismul se naște din dorul de absolut, dintr-o neliniște interioară care nu se poate exprima prin imagini clare sau idei logice, ci doar prin simboluri, sunete, culori, ecouri. Este limbajul sufletului care nu mai vrea să spună direct, ci să sugereze, să insinueze, să învăluie. Este arta de a transforma realitatea într-un vis, iar visul într-o presimțire.
+</p><p>În inima simbolismului bate o nevoie profundă de evadare: din convenții, din cotidian, din rigiditatea cuvântului precis. Poetul simbolist e un visător care simte lumea nu ca pe un decor, ci ca pe o vibrație, o corespondență între nevăzut și văzut, între ceea ce este și ceea ce s-ar putea simți.</p>`,
+    image: "res/death.jpg"
+  },
+  trasaturi: {
+    title: "Trăsături ale Simbolismului",
+    text: `<p>Simbolismul nu vorbește direct – el sugerează. Prima și poate cea mai profundă trăsătură a acestui curent este tocmai arta sugestiei. Poetul simbolist nu descrie în mod clar și precis, ci lasă emoțiile să se strecoare printre cuvinte, ca lumina printre frunze. Sensurile sunt voalate, ambigue, uneori visate mai degrabă decât înțelese.</p>
+<p>O altă trăsătură esențială este folosirea simbolurilor, a imaginilor care spun mai mult decât ceea ce par. Un „corb” poate fi moartea, o „vioară” poate fi sufletul însingurat, „ploaia” poate fi deznădejdea eternă. Totul devine limbaj dublu – ceea ce vezi și ceea ce simți dincolo de vedere.</p>
+<p>Simbolismul este o poezie a muzicalității. Nu doar rima contează, ci mai ales ritmul, sonoritatea, repetițiile, aliterațiile și asonanțele. Cuvintele curg ca într-o simfonie interioară, menită să transmită stări, nu doar idei. E poezie care se aude, se simte, se respiră.</p>
+<p>O lume interioară domină totul: melancolia, tristețea, neliniștea, refugiul în vis. Poetul simbolist este adesea un suflet rănit, obosit de realitate, care caută frumusețea într-o lume ideală, uneori decadentă, dar mereu încărcată de mister.</p>
+<p>Prezența culorilor cu valoare afectivă este o altă trăsătură marcantă. Galbenul devine boală, violetul – visare, negrul – moarte sau angoasă. Culorile nu mai sunt doar vizuale, ci trăiri condensate în nuanțe.</p>
+<p>Simbolismul cultivă corespondențele – legături tainice între sunete, mirosuri, culori, sentimente. Este influențat de ideea că totul în univers este conectat prin fire invizibile, iar poezia poate prinde această muzică secretă a lumii.</p>
+<p>În cele din urmă, simbolismul se îndepărtează de exterior pentru a explora universul interior, subconștientul, stările sufletești difuze, visul, intuiția, spaima fără nume. Este o coborâre în adâncul ființei, acolo unde logica nu mai are putere, dar unde poezia devine adevăr.</p>`,
+    image: "res/cessare.jpg"
+  },
+  simbolisti_francezi: {
+    title: "Simboliști francezi - 4 reprezentanți",
+    text: `<p>Simbolismul a luat naștere în Franța, iar principalii săi reprezentanți au fost Charles Baudelaire, Paul Verlaine, Stéphane Mallarmé și Arthur Rimbaud. Aceștia au transformat poezia într-un act de introspecție profundă și rafinament stilistic. Fiecare poet a adus o contribuție unică: Baudelaire a introdus ideea de corespondențe, Verlaine a creat o poezie muzicală și vagă, Mallarmé a dus simbolismul în abstract și ermetism, iar Rimbaud a sfărâmat convențiile poetice prin viziuni intense și halucinante.</p>`,
+    image: "res/camp.jpeg"
+  },
+  baudelaire: {
+    title: "Charles Baudelaire",
+    text: `<p>Baudelaire este considerat părintele simbolismului. Prin volumul Florile răului, el a impus o poezie a contrastelor: frumusețea și urâtul, idealul și spleen-ul, erosul și moartea. Baudelaire nu descrie, ci sugerează. El transformă orașul, trupul, păcatul și disperarea în poezie. Versurile sale sunt muzicale, dense, încărcate de simboluri și ambiguitate. Poetul devine un „albatros” , ființă superioară dar inadaptată lumii.</p>`,
+    image: "res/Baudelaire.png"
+  },
+  verlaine: {
+    title: "Paul Verlaine",
+    text: `<p> Verlaine este poetul care a spus: „muzica, înainte de toate”. Poezia lui este delicată, melancolică și plină de sugestii. El nu caută să spună lucruri clare, ci să trezească emoții prin sunet și atmosferă. A cultivat imagini estompate, stări vagi, iubiri pierdute. Viața lui agitată și sensibilitatea fragilă se regăsesc în versuri ce par mai aproape de muzică decât de rațiune. Este vocea blândă a simbolismului francez.  </p>`,
+    image: "res/paul.png"
+  },
+  mallarme: {
+    title: "Stéphane Mallarmé",
+    text: `<p> Mallarmé este cel mai abstract poet simbolist. Pentru el, poezia nu trebuie să explice nimic, ci să sugereze totul. Scria ca și cum ar construi o vrajă: cuvintele nu au un înțeles imediat, dar creează o stare. A fost obsedat de perfecțiunea limbajului și de ideea că poezia trebuie să fie pură. Versurile lui sunt greu de pătruns, dar fascinante. El a văzut în poezie o formă de revelație și tăcere. </p>`,
+    image: "res/stephane mallarme.jpeg"
+  },
+  rimbaud: {
+    title: "Arthur Rimbaud",
+    text: `<p> Rimbaud este poetul vizionar, adolescentul revoltat care a ars intens și a renunțat devreme la poezie. A vrut să acceseze adevărul prin dereglarea simțurilor. Poezia sa e halucinantă, plină de culori, sunete, forme bizare. A scris Iluminări, Un sezon în infern și poemul Corabia beată. Rimbaud a dus simbolismul spre extrem: a distrus formele fixe și a scris cu o libertate aproape nebună. </p>`,
+    image: "res/arthur rimbaud.jpeg"
+  },
+  florile_raului: {
+    title: "Florile răului - Baudelaire",
+    text: `<p> Florile răului de Charles Baudelaire este cartea care a schimbat fața poeziei moderne. Este un volum despre contradicțiile sufletului omenesc: dorință și vină, ideal și decădere, iubire și ură. Poeziile sunt grupate tematic și tratează teme ca spleen-ul, orașul, moartea, păcatul. Volumul a fost cenzurat la apariție, dar astăzi este considerat capodopera simbolismului. Cuvintele sunt alese cu o precizie muzicală, iar imaginile sunt uneori șocante, alteori sublime. </p>`,
+    image: "res/flori.jpeg"
+  },
+  corbul: {
+    title: "Motivul Corbului și Răul Secolului",
+    text: `<p> Corbul este simbolul întunecat al morții, al destinului implacabil și al singurătății. Apare în literatura simbolistă ca o prezență care planează peste suflet. Deși nu apare direct la Baudelaire, atmosfera poeziei sale îl sugerează. În cultura simbolistă, corbul devine un mesager al disperării, un ecou al fricii interioare. Inspirat din The Raven de Edgar Allan Poe, corbul spune mereu „Nevermore” ,niciodată. </p>`,
+    image: "res/raven.jpg"
+  },
+  poet_decadent: {
+    title: "Conceptul de Poet Decadent",
+    text: `<p> Poetul decadent este un artist al extremelor: rafinat, dar obosit de lume; sensibil, dar cinic. El trăiește într-o criză de valori și respinge realitatea banală. În loc să celebreze frumusețea convențională, preferă estetica urâtului, a misterului, a morții. Baudelaire, Verlaine și Huysmans sunt figuri centrale. Poezia decadentă nu e moralizatoare, ci fascinată de ceea ce este interzis, interioară, uneori bolnavă , dar mereu intensă.  </p>`,
+    image: "res/decay.jpeg"
+  },
+  simbolism_romanesc: {
+    title: "Simbolismul românesc",
+    text: `<p> Simbolismul românesc s-a inspirat din cel francez, dar a dezvoltat o voce proprie. A apărut la sfârșitul secolului XIX, în opoziție cu realismul și romantismul. Poeții români au folosit simboluri, sinestezii, muzicalitate și teme precum melancolia, evadarea, moartea și iubirea pierdută. În România, simbolismul are și o nuanță națională: natura, orașul provincial, toamna balcanică. Este un refugiu din cotidian într-o lume a visului. </p>`,
+    image: "res/roman.jpeg"
+  },
+  reprezentanti_romani: {
+    title: "Reprezentanți români: Minulescu, Anghel, Macedonski, Petică",
+    text: `<p> Simbolismul românesc are mai mulți poeți importanți:</p>
+<p>Alexandru Macedonski : inițiatorul simbolismului la noi</p>
+<p>Ion Minulescu : poetul orașului și al iubirii romantice</p>
+<p>George Bacovia : maestrul atmosferei apăsătoare și al spleen-ului</p>
+<p>Dimitrie Anghel : sensibil, legat de natură și melancolie</p>
+<p>Ștefan Petică : visător, rafinat, aproape mistic în trăiri</p>
+<p>Toți au transformat poezia română, dându-i profunzime și muzicalitate.</p>`,
+    image: "res/full.png"
+  },
+  minulescu: {
+    title: "Ion Minulescu",
+    text: `<p> Minulescu a fost un poet al teatrului sufletesc. Scria despre oraș, despre iubiri eșuate, despre melancolie cu zâmbet amar. Poeziile lui au o notă de spectacol: sunt romanțe urbane, cu personaje fantomatice, tramvaie, umbre și dor. El folosește ironia și simbolurile ca un actor pe o scenă goală. Chiar și în durere, versurile lui păstrează o grație aparte.  </p>`,
+    image: "res/ion minulescu.jpeg"
+  },
+  anghel: {
+    title: "Dimitrie Anghel",
+    text: `<p> Dimitrie Anghel scrie o poezie discretă, delicată, plină de simboluri naturale: frunze, ramuri, lumini difuze. Este un poet al melancoliei și al stărilor sufletești tăcute. Natura reflectă emoția, iar imaginile lui nu descriu peisaje, ci suflete. Poezia lui nu are dramatism, dar transmite o sensibilitate profundă. </p>`,
+    image: "res/dimitrie anghel.jpeg"
+  },
+  macedonski: {
+    title: "Alexandru Macedonski",
+    text: `<p> Macedonski este deschizătorul simbolismului în România. A introdus forme noi, precum rondelul, și a scris poezie abstractă, plină de idei și imagini alegorice. În Noaptea de decemvrie, descrie lupta dintre orgoliu și renunțare printr-o alegorie vizuală. Macedonski a fost un poet vizionar, adesea neînțeles, dar esențial în literatura noastră.  </p>`,
+    image: "res/alexandru machedonski.jpeg"
+  },
+  petica: {
+    title: "Ștefan Petică",
+    text: `<p> Petică este cel mai „pur” simbolist român. Poezia sa e muzicală, albă, visătoare, fără greutate lumii reale. În Fecioara în alb, totul este rafinament și ideal. El fuge de concret și caută armonia, liniștea interioară, absolutul. Este un poet filozofic, care transformă poezia într-o formă de rugăciune estetică.  </p>`,
+    image: "res/petica.jpeg"
+  },
+  sansonetist: {
+    title: "Simbolismul Șansonetist",
+    text: `<p> Este un tip de poezie simbolistă cu tentă urbană și teatrală, inspirată din cântecele de cafenea (șansonete). Tonul este ușor, muzical, dar ascunde o melancolie adâncă. Minulescu e reprezentantul principal. Poezia devine un spectacol al singurătății, cu domni și doamne elegante, iubiri apuse, becuri palide și gesturi dramatice. E poezia care zâmbește trist.  </p>`,
+    image: "res/sanson.png"
+  },
+  bacovia: {
+    title: "George Bacovia - Poezie și Lirism",
+    text: `<p> Bacovia este vocea spleen-ului românesc. Poezia lui este grea, cenușie, obosită. Ploaia, toamna, școala părăsită, orașul pustiu , toate devin simboluri ale alienării. Scrie simplu, dar apăsător. Bacovia nu se plânge , doar constată, cu o luciditate amară. El nu scrie poezie, ci respiră tristețe pe hârtie. </p>`,
+    image: "res/bacovia.jpeg"
+  },
+  instrumente_culori: {
+    title: "Instrumente și culori la Bacovia",
+    text: `<p>În simbolism, instrumentele (vioară, harpă) și culorile (negru, galben, violet) exprimă emoții. Nu sunt simple decoruri, ci glasul sufletului. Vioara plânge, violetul visează, galbenul doare. Bacovia folosește aceste elemente pentru a sugera anxietatea, singurătatea, spleen-ul. Culorile nu se văd ,se simt.  </p>`,
+    image: "res/culori.png"
+  },
+  simbolul_plumbului: {
+    title: "Simbolul Plumbului la Bacovia",
+    text: `<p> La Bacovia, plumbul devine esența unei lumi sufocate. Este simbolul morții interioare, al iubirii moarte, al vieții trăite în gol. Versul „dormea întors amorul meu de plumb” e poate cea mai grea poezie din literatura română , în doar câteva cuvinte, sufletul se închide într-un sicriu emoțional.  </p>`,
+    image: "res/plumb.jpeg"
+  },
+  poezie_bacovia: {
+    title: "Reflexia poeziei în lirica bacoviană",
+    text: `<p> Lirica lui Bacovia este expresia unei existențe apăsătoare, fără iluzii. Poeziile sale nu strigă – șoptesc. Ele descriu o lume cenușie, în care sufletul obosește, iubește în tăcere, moare pe dinăuntru. Este o poezie a frigului, a golului, dar și a sincerității. Bacovia a făcut din spleen o artă. </p>`,
+    image: "res/lirica.png"
+  }
+};
+
+let currentKey = null; 
+
+function showContent(key) {
+  const box = document.getElementById("contentBox");
+
+  if (currentKey === key && box.style.display === "block") {
+    box.style.display = "none";
+    currentKey = null;
+    return;
+  }
+
+  const content = contents[key];
+  document.getElementById("contentTitle").textContent = content.title;
+  document.getElementById("contentText").innerHTML = `<p>${content.text}</p>`;
+
+  const imageElement = document.getElementById("image");
+  if (content.image) {
+    imageElement.src = content.image;
+    imageElement.style.display = "block";
+  } else {
+    imageElement.style.display = "none";
+  }
+
+  box.style.display = "block";
+  window.scrollTo({
+    top: box.offsetTop - 20,
+    behavior: "smooth"
+  });
+
+  currentKey = key;
+}
+
+
+const toggleBtn = document.getElementById('menuToggle');
+const overlay = document.getElementById('overlayMenu');
+
+toggleBtn.addEventListener('click', function () {
+  if (overlay.classList.contains('show')) {
+    overlay.classList.remove('show');
+    toggleBtn.innerHTML = '<img src="open-book (1)-Photoroom.png" alt="Open Book" style="width:30px;"/>';
+  } else {
+    overlay.classList.add('show');
+    toggleBtn.textContent = '✕';
+  }
+});
+const toggleleftBtn = document.getElementById('menuToggleleft');
+const overlayleft = document.getElementById('overlayleftMenu');
+
+toggleleftBtn.addEventListener('click', function () {
+  if (overlayleft.classList.contains('show')) {
+    overlayleft.classList.remove('show');
+    toggleleftBtn.innerHTML = '<img src="music.png" alt="Open Book" style="width:30px"/>';
+  } else {
+    overlayleft.classList.add('show');
+    toggleleftBtn.textContent = '✕';
+  }
+});
+
